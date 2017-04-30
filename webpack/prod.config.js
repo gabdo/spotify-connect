46d0eb6a99c33050a199f5339bc75636c106d69e
gabdo <carlos.dominguez@globant.com>
2017-04-30 12:52:57 -0300

    Update prod.config.js

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -22,10 +22,10 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: [
-            'babel-preset-es2015',
-            'babel-preset-react',
-            'babel-preset-stage-0'
-          ].map(require.resolve),
+            'es2015',
+            'react',
+            'stage-0'
+          ],
         },
       },
     ],
